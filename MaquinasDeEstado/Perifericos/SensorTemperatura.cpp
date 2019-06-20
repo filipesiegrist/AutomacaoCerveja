04511f272ba8e80fc02f1c69fbfc5a2990da2f6e
filipesiegrist <filipebrasil8@gmail.com>
2019-06-20 15:44:08 -0300

    Adicionando as verificacoes de limite de temperatura.

--- MaquinasDeEstado/Perifericos/SensorTemperatura.cpp
+++ MaquinasDeEstado/Perifericos/SensorTemperatura.cpp
@@ -2,7 +2,7 @@
 * @Author: Filipe Ernesto Siegrist Gonçalves
 * @Date:   2019-06-20 10:20:49
 * @Last Modified by:   filipe-ufsc
-* @Last Modified time: 2019-06-20 11:56:48
+* @Last Modified time: 2019-06-20 15:43:16
 */
 
 #ifndef _SENSOR_TEMPERATURA_CPP_
@@ -10,6 +10,12 @@
 
 #include "SensorTemperatura.h"
 
+SensorTemperatura::passou_do_limite_maximo(void) {
+	return (bool) leitura() > temperatura_maxima;
+}
 
+SensorTemperatura::passou_do_limite_minimo(void) {
+	return (bool) leitura() < temperatura_minima;
+}
 
 #endif
