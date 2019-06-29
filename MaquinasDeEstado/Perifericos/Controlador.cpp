25ca8e523277a7008a1d64b6546e07abaeb99302
filipesiegrist <filipebrasil8@gmail.com>
2019-06-29 12:24:45 -0300

    Implantando a rotina de controle

--- MaquinasDeEstado/Perifericos/Controlador.cpp
+++ MaquinasDeEstado/Perifericos/Controlador.cpp
@@ -2,7 +2,7 @@
 * @Author: Filipe Ernesto Siegrist Gonçalves
 * @Date:   2019-06-20 10:21:45
 * @Last Modified by:   filipe-ufsc
-* @Last Modified time: 2019-06-29 12:00:34
+* @Last Modified time: 2019-06-29 12:15:57
 */
 
 #ifndef _CONTROLADOR_CPP_
@@ -12,6 +12,8 @@
 
 #include "Controlador.h"
 #include "SensorTemperatura.h"
+
+#define TRUE 1
 
 int Controlador::desvio_de_temperatura(int temperatura_medida) {
 	return temperatura_referencia - temperatura_medida;
@@ -27,17 +29,22 @@ int Controlador::saida_duty_cicle(int temperatura_medida) {
 	return saida;
 }
 Controlador(int temp_objetivo) {
-	pinMode(AQUECEDOR, OUTPUT);
 	sensor.temperatura_objetivo = temp_objetivo;
 	temperatura_referencia = temp_objetivo;
 }
 void Controlador::controla_temperatura() {
-
+	while (TRUE) {
+		int temperatura = sensor.leitura();
+		int duty_cicle = saida_duty_cicle(temperatura);
+		int saida = map(duty_cicle, 0, 1, 0, 1023);
+		analogWrite(AQUECEDOR, saida);
+		delay(20);
+	}
 }
 
-void Controlador::~Controlador() {
-	// Desliga o aquecedor
-	digitalWrite(AQUECEDOR,LOW);
-}
+//void Controlador::~Controlador() {
+//	// Desliga o aquecedor
+//	digitalWrite(AQUECEDOR,LOW);
+//}
 
 #endif
