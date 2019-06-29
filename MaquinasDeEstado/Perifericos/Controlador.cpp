5f5561a749db968d33f0c925d5339bba0c28fd82
filipesiegrist <filipebrasil8@gmail.com>
2019-06-29 11:45:14 -0300

    Implemntando a logica de controle

--- MaquinasDeEstado/Perifericos/Controlador.cpp
+++ MaquinasDeEstado/Perifericos/Controlador.cpp
@@ -2,7 +2,7 @@
 * @Author: Filipe Ernesto Siegrist Gonçalves
 * @Date:   2019-06-20 10:21:45
 * @Last Modified by:   filipe-ufsc
-* @Last Modified time: 2019-06-20 16:19:10
+* @Last Modified time: 2019-06-29 11:44:46
 */
 
 #ifndef _CONTROLADOR_CPP_
@@ -11,14 +11,17 @@
 #include "Controlador.h"
 
 int Controlador::desvio_de_temperatura(int temperatura_medida) {
-
+	return temperatura_referencia - temperatura_medida;
 }
 
 int Controlador::calcula_duty_cicle(int temperatura_medida) {
-
+	return Kp*desvio_de_temperatura(temperatura_medida);
 }
 int Controlador::saida_duty_cicle(int temperatura_medida) {
-
+	int saida = calcula_duty_cicle(temperatura_medida);
+	if (saida > 1) saida = 1;
+	else if (saida < 0) saida = 0;
+	return saida;
 }
 Controlador() {
 
