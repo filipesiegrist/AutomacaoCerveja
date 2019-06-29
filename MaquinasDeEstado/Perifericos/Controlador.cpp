783ffae92f7a8004ba98acd7cf766b8060a19f2a
filipesiegrist <filipebrasil8@gmail.com>
2019-06-29 12:45:26 -0300

    Implementando o controlador com timer

--- MaquinasDeEstado/Perifericos/Controlador.cpp
+++ MaquinasDeEstado/Perifericos/Controlador.cpp
@@ -2,7 +2,11 @@
 * @Author: Filipe Ernesto Siegrist Gonçalves
 * @Date:   2019-06-20 10:21:45
 * @Last Modified by:   filipe-ufsc
+<<<<<<< Updated upstream
 * @Last Modified time: 2019-06-20 16:19:10
+=======
+* @Last Modified time: 2019-06-29 12:43:30
+>>>>>>> Stashed changes
 */
 
 #ifndef _CONTROLADOR_CPP_
@@ -27,7 +31,22 @@ void Controlador::inicio(void) {
 
 }
 void Controlador::controla_temperatura() {
+<<<<<<< Updated upstream
 
+=======
+	int tempo_inicial = millis();
+	int tempo_atual = millis();
+	int delta_t = tempo_atual - tempo_inicial;
+	while (delta_t < TEMPO_BRASSAGEM) {
+		delay(20);
+		int temperatura = sensor.leitura();
+		int duty_cicle = saida_duty_cicle(temperatura);
+		int saida = map(duty_cicle, 0, 1, 0, 1023);
+		analogWrite(AQUECEDOR, saida);
+		tempo_atual = millis();
+		delta_t = tempo_atual - tempo_inicial;
+	}
+>>>>>>> Stashed changes
 }
 void Controlador::fim(void) {
 
--- MaquinasDeEstado/Perifericos/Controlador.h
+++ MaquinasDeEstado/Perifericos/Controlador.h
@@ -14,8 +14,14 @@
 #define Kp 1
 
 //Pino de atuacao PWM
-#define AQUECEDOR 5
+#define AQUECEDOR 5A1
 
+<<<<<<< Updated upstream
+=======
+#define TEMPERATURA_PADRAO 65
+#define TEMPO_BRASSAGEM 50*60*1000 // ms, ou 50 minutos
+
+>>>>>>> Stashed changes
 class Controlador {
 private:
 
