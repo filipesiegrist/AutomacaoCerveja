d7d78414600a57edc6c4cbe35dd1eab701d094aa
Paulo Victor <paulovictor237@gmail.com>
2019-06-29 16:43:00 -0300

    Implementado os botoes como perifericos

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -2,6 +2,7 @@
 #include <Controlador.h>
 #include <Leds.h>
 #include <Bomba.h>
+#include <Botao.h>
 
 
 void setup() {
@@ -11,17 +12,17 @@ void setup() {
   // put your setup code here, to run once:
   //controlador_brassagem.controla_temperatura();
   //controlador_brassagem.esquenta(80);
-  Bomba bomba_mistura;
-  bomba_mistura.liga();
-  delay(8000);
-  bomba_mistura.desliga();
-  delay(5000);
-  bomba_mistura.liga();
-  delay(5000);
+  iniciaBotoes();
+  
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  Bomba bomba_mistura;
+  esperaAcaoUsuario(BOTAO_ACAO);
+  bomba_mistura.liga();
+  esperaAcaoUsuario(BOTAO_ON_OFF);
+  bomba_mistura.desliga();
+  
 }
---
+++ MaquinasDeEstado/Perifericos/Botao.cpp
@@ -0,0 +1,26 @@
+/*
+* @Author: Filipe Ernesto Siegrist Gonçalves
+* @Date:   2019-06-20 10:27:30
+* @Last Modified by:   filipe-ufsc
+* @Last Modified time: 2019-06-20 11:56:07
+*/
+
+#ifndef _BOTAO_CPP_
+#define _BOTAO_CPP_
+
+#include <Arduino.h>
+
+void iniciaBotoes() {
+  pinMode(BOTAO_ACAO,INPUT);
+  pinMode(BOTAO_ON_OFF,INPUT);
+}
+
+void esperaAcaoUsuario(int botao) {
+  int valor = digitalRead(botao);
+  while (valor == LOW) {
+    valor = digitalRead(botao);
+    delay(10);
+  }
+}
+
+#endif
